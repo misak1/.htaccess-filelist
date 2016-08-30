1f89d4269935ab741b3b4fc17fa98730f68929b8
Misaki Shibata <m-shibata@imjp.co.jp>
2016-08-30 19:38:53 +0900

    Create file-icon.js

---
+++ file-icon.js
@@ -0,0 +1,20 @@
+$('tr').each(function(i, elem) {
+    var $td = $(elem).find('td').eq(1);
+    var filename = $td.text();
+    if(/\.zip/.test(filename)){
+        console.log(filename);
+        $td.prepend('<i class=\'sprite_page_white_zip\'></i>');
+    }
+    if(/\.pdf/.test(filename)){
+        console.log(filename);
+        $td.prepend('<i class=\'sprite_page_white_acrobat\'></i>');
+    }
+    if(/\.xls/.test(filename)){
+        console.log(filename);
+        $td.prepend('<i class=\'sprite_page_excel\'></i>');
+    }
+    if(/\//.test(filename)){
+        console.log(filename);
+        $td.prepend('<i class=\'sprite_folder\'></i>');
+    }
+});
